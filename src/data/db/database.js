785cd68cdeace2917b407f30febd7f1729b37f64
Rebraxin <cedric.paje@gmail.com>
2020-08-23 12:12:39 +0200

    contact from

--- src/data/db/database.js
+++ src/data/db/database.js
@@ -370,9 +370,9 @@ const database = {
     },
   ],
   contactInfo: {
-    phoneNumbers: ['+012-3456-7891', '+012-3456-7892'],
-    emailAddress: ['admin.sitename@example.com', 'info.sitename@example.com'],
-    address: '121 King Street, Melbourne, Victoria 3000, Australia',
+    phoneNumbers: ['+33-637-964-370'],
+    emailAddress: ['cedric.paje@gmail.com'],
+    address: '340 rue de l\'Europe, 01630 Saint-Genis-Pouilly, France',
   },
 };
 
